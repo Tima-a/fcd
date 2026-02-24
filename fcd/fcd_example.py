3348c04d87c43b0e103097b56c68ebcfa62be1cc
Teymur Aghayev <107584760+Tima-a@users.noreply.github.com>
2026-02-24 23:06:09 +0200

    First version upload

---
+++ fcd/fcd_example.py
@@ -0,0 +1,26 @@
+from mode_fitting import FCD
+import utility
+import utility_guesses
+import numpy as np
+
+#Set datasets
+y=np.load(f"test_datasets/cryptocoin_tests/test18.npy")[:1000]
+x=np.arange(len(y))
+
+#Initialize FCD runner
+fcd = FCD(
+    x_dataset=x, y_dataset=y,
+    model=utility.model_sin6,
+    initial_guesses_function=utility_guesses.initial_guess_sin6,
+    parallel=True,
+    verbose=1
+)
+
+# Execute fitting
+params = fcd.run()
+
+# Extract analytic insights
+fcd.print_fitted_functions()
+fitted_y_values=fcd.calculate_y_fit_modes()
+derivatives = fcd.calculate_derivatives(order=1, print_derivative_formulas=True)
+integrals = fcd.calculate_integrals(order=1, print_integral_formulas=True)
